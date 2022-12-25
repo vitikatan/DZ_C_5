cb88c6afe9cae1eb22559f3c6fd153c28f0fc532
Yana <wyemelt@gmail.com>
2022-12-25 18:23:24 +0300

    Zadacha_36 done

---
+++ Zadacha_36/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите длину массива:");
+int Lon = int.Parse(Console.ReadLine());
+
+int[] array = new int[Lon];
+Random rand = new Random();
+for (int i = 0; i < Lon; i++)
+    array[i] = rand.Next(-100, 100);
+var str = string.Join(" ", array);
+Console.WriteLine(str);
+
+int cht = 0;
+for (int i = 0; i < Lon; i = i + 2)
+ cht = array[i] + cht;
+Console.WriteLine($"Сумма нечетных элементов = {cht}");
