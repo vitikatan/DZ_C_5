f1dc1e3300894e37886536ee365378cf0183ce76
Yana <wyemelt@gmail.com>
2022-12-25 18:14:34 +0300

    Zadacha_34 done

---
+++ Zadacha_34/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите длину массива:");
+int Lon = int.Parse(Console.ReadLine());
+
+int[] array = new int[Lon];
+Random rand = new Random();
+for (int i = 0; i < Lon; i++)
+    array[i] = rand.Next(100, 1000);
+var str = string.Join(" ", array);
+Console.WriteLine(str);
+
+int cht = 0;
+for (int i = 0; i < Lon; i++)
+    if (array[i] % 2 == 0) cht++;
+Console.WriteLine($"Количество четных чисел в массиве = {cht}");
